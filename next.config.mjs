4966f3875e568a4a71df48b42332e9d28423f1f1
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2025-09-06 16:33:03 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
+  distDir: "build",
+
 }
 
 export default nextConfig
